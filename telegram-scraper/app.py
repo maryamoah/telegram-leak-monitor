54071b705dd464b081e91f595cc2a78847c09a03
Mary Abenawa Techiesiwa Amoah <46970579+maryamoah@users.noreply.github.com>
2025-12-09 10:06:20 +0400

    Update app.py

--- telegram-scraper/app.py
+++ telegram-scraper/app.py
@@ -35,9 +35,3 @@ async def start_scraper():
 with client:
     client.loop.run_until_complete(start_scraper())
     
-if __name__ == "__main__":
-    import time
-    print("Scraper container started. Waiting...")
-    while True:
-        time.sleep(10)
-    
