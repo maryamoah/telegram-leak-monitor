15845f171e20bae828b31e673bd0537f3851184d
Mary Abenawa Techiesiwa Amoah <46970579+maryamoah@users.noreply.github.com>
2025-12-29 14:41:46 +0400

    Update entrypoint.sh

--- extractor-engine/entrypoint.sh
+++ extractor-engine/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 
-echo "[+] Starting extractor API (Flask)"
+echo "[+] Starting extractor Flask app..."
 python /app/app.py &
 
-echo "[+] Starting cron scheduler (supercronic)"
+echo "[+] Starting cron scheduler..."
 exec supercronic /etc/cronjobs
